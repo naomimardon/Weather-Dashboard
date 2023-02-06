a363ef6a5f41eca698b8042af60725f74bfb2782
Naomi Mardon <naomimardon@Naomis-MBP.broadband>
2023-02-06 14:04:32 +0000

    updated JS to call data from Open Weather 5 Day Forcast API based on lon and lat converted from city name by geocoding API

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,9 +28,38 @@ function storeCitiesArray() {
     localStorage.setItem("Cities", JSON.stringify(cities));
 };
 
+function callWeatherData(weatherResponse) {
+    console.log(weatherResponse);
+    console.log(weatherResponse.list[0].main.temp);
+}
+
+function buildQueryURL() {
+const APIKey = "2befb069531c6856f267a412d5ad148c";
+let cityInput = $("#search-input").val().trim().toLowerCase();
+
+lonLatURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityInput + "&limit=1&appid=" + APIKey;
+
+    $.ajax({
+        url: lonLatURL,
+        method: "GET"
+    }).then(function(geoResponse) {
+        let lon = geoResponse[0].lon;
+        let lat = geoResponse[0].lat;
+        const queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=metric&appid=" +APIKey;
+        
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(weatherResponse) {
+            callWeatherData(weatherResponse);
+        });
+    });
+
+};
+
 $("#search-button").on("click", function(event) {
     event.preventDefault();
-    let cityInput = $("#search-input").val().trim();
+    let cityInput = $("#search-input").val().trim().toUpperCase();
     cities.push(cityInput);
     if (cities.length > 5) {
         cities.shift();
@@ -38,6 +67,7 @@ $("#search-button").on("click", function(event) {
 
     renderCityButtons();
     storeCitiesArray();
+    buildQueryURL();
 
     cityInput = $("#search-input").val("");
 });
