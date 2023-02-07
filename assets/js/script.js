0120468fcc70d8d4afe9efd99e14cd20f88c73b7
Naomi Mardon <naomimardon@Naomis-MBP.broadband>
2023-02-07 10:05:19 +0000

    updated JS to render 5 day forecast when search button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,22 +40,80 @@ function renderTodaysWeather(weatherResponse) {
     todaysHeader.text(cityInput + " " + todaysDate);
 
     let todaysIconID = weatherResponse.list[0].weather[0].icon;
-    let iconURL = 'https://openweathermap.org/img/wn/'+ todaysIconID + '@2x.png';
+    let iconURL = "https://openweathermap.org/img/wn/"+ todaysIconID + "@2x.png";
     let todaysIcon = $("<img>")
         .attr("src", iconURL);
     todaysHeader.append(todaysIcon);
 
     let todaysTemp = $("#todaysTemp");
-    todaysTemp.text("Temperature: " + weatherResponse.list[0].main.temp + "°C");
+    todaysTemp.text("Temperature: " + parseInt(weatherResponse.list[0].main.temp) + "°C");
 
     let todaysWind = $("#todaysWind");
-    let todaysWindSpeed = Math.round((weatherResponse.list[0].wind.speed *10) *100) /100;
-    todaysWind.text("Wind Speed: " + todaysWindSpeed + "km/hr");
+    let windSpeed = parseInt((weatherResponse.list[0].wind.speed) * 3.6);
+    todaysWind.text("Wind Speed: " + windSpeed + " km/h");
 
     let todaysHumidity = $("#todaysHumidity");
     todaysHumidity.text("Humidity: " + weatherResponse.list[0].main.humidity + "%");
 
     cityInput = $("#search-input").val("");
+}
+
+function renderForecast(weatherResponse) {
+    console.log(weatherResponse);
+
+    $("#forecastHeaderRow").empty();
+    $("#forecastRow").empty();
+
+    let forecastHeader = $("<h3>")
+        .attr("id", "forecastHeader")
+        .css("padding-left", "20px")
+        .text("5 Day Forecast:")
+    $("#forecastHeaderRow").append(forecastHeader);
+
+    forecastArray = [weatherResponse.list[8], weatherResponse.list[16], weatherResponse.list[24], weatherResponse.list[32], weatherResponse.list[39]]
+    console.log(forecastArray);
+    forecastArray.forEach(function (forecast) {
+        let dayCard = $("<div>")
+            .addClass("card dayCard");
+
+        let dayIconID = forecast.weather[0].icon;
+        let dayIconURL = "https://openweathermap.org/img/wn/"+ dayIconID + "@2x.png";
+        let dayIcon = $("<img>")
+            .addClass("card-img-top dayIcon")
+            .attr("src", dayIconURL);
+
+        let dayHeader = $("<h4>")
+            .addClass("dayHeader");
+        let date = moment(forecast.dt_txt, "YYYY-MM-DD hh:mm:ss").format("DD/MM/YYYY")
+        dayHeader.text(date);
+
+
+        let dayTemp = $("<p>")
+            .addClass("card-text dayPTag")
+            .attr("id", "dayTemp");
+        let temp = parseInt(forecast.main.temp);
+        dayTemp.text("Temp: " + temp + "°C");
+        
+        let dayWind = $("<p>")
+            .addClass("card-text dayPTag")
+            .attr("id", "dayWind");
+        let wind = parseInt((forecast.wind.speed) *3.6);
+        dayWind.text("Wind: " + wind + " km/h");
+        
+        let dayHumidity = $("<p>")
+            .addClass("card-text dayPTag")
+            .attr("id", "dayHumidity");
+        let humidity = forecast.main.humidity;
+        dayHumidity.text("Humidity: " + humidity + "%");
+        
+        dayCard.append(dayIcon);
+        dayCard.append(dayHeader);
+        dayCard.append(dayTemp);
+        dayCard.append(dayWind);
+        dayCard.append(dayHumidity);
+
+        $("#forecastRow").append(dayCard);
+    });
 }
 
 function buildQueryURL() {
@@ -79,9 +137,9 @@ console.log("lonLatURL: " + lonLatURL);
             method: "GET"
         }).then(function(weatherResponse) {
             renderTodaysWeather(weatherResponse);
+            renderForecast(weatherResponse);
         });
     });
-
 };
 
 $("#search-button").on("click", function(event) {
